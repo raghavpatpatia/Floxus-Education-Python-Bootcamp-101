79a3ea664a9c80e6cb4bd6c4537bd668c344ac80
Raghav <63911252+raghavpatpatia@users.noreply.github.com>
2021-07-03 00:43:49 +0530

    Create Power.py

--- Assignment 1/Power.py
+++ Assignment 1/Power.py
@@ -1,8 +1,3 @@
 n = int(input("Enter power of two: "))
-<<<<<<< HEAD
-powerOfTwo = 2 << (n - 1)
-print("2 ^", n, "is:", powerOfTwo)
-=======
 powerOfTwo = 2 << (n-1)
-print("2 ^", n, "is:", powerOfTwo)
->>>>>>> 68877e356ead3d53622398b2b91c700b6641e9d3
+print(powerOfTwo)
